89a08668635639f935139c4485471c4526d7165e
felya_97@mail.ru <felya_97@mail.ru>
2022-01-11 00:49:39 +0600

    First Commit

---
+++ src/main/java/com/example/springnema/SpringNemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNemaApplication.class, args);
+    }
+
+}
